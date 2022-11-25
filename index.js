e9a82e204dae3d7cdc8060e47382d25df843802f
ayub2022 <ayubmaina2017@gmail.com>
2022-11-25 22:12:57 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+function receivesAFunction(callback){
+    let greetings = 'Sasa '
+   
+   callback(greetings)
+   
+   }
+   
+   function callback(greetings){
+    console.log(greetings)
+   }
+   
+   function returnsANamedFunction() {
+       return function sayHello(){
+            console.log('returned the function')
+       }
+   }
+   
+   function returnsAnAnonymousFunction(){
+       return ()=>{
+           console.log('Hello world')
+       }
+   }
+   
